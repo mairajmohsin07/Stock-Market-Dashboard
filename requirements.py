ac1ead40c72fbb57126ccae99fe892963819198f
mairajmohsin07 <mairaj.mohsin07@gmail.com>
2024-05-21 14:54:03 -0400

    Required Packages

---
+++ requirements.py
@@ -0,0 +1,15 @@
+dash==2.1.0
+dash-bootstrap-components==1.0.0
+dash-core-components==2.1.0
+dash-html-components==2.0.0
+dash-renderer==2.0.0
+dash-table==5.0.0
+datetime
+numpy
+pandas
+plotly==5.3.1
+pandas_ta
+yfinance
+newspaper3k
+requests
+beautifulsoup4
