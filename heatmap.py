9c4a1bdaa71017753ffe0d9fbe5354835337d0df
mairajmohsin07 <mairaj.mohsin07@gmail.com>
2024-05-21 14:51:48 -0400

    Heatmaps Visualization

---
+++ heatmap.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import yfinance as yf
+import plotly.express as px
+
+CACHE_SP500 = 'sp500_cache.pkl'
+CACHE_NASDAQ = 'nasdaq_cache.pkl'
+CACHE_DOWJONES = 'dowjones_cache.pkl'
+
+def sp_500(val):
+    # S&P 500 heatmap
+    pass
+
+def nasdaq(val):
+    # NASDAQ heatmap
+    pass
+
+def dowjones(val):
+    # Dow Jones heatmap
+    pass
