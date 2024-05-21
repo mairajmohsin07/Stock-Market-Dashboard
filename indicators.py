714367d464b72a68f660b2baf1acfc242ebb3c12
mairajmohsin07 <mairaj.mohsin07@gmail.com>
2024-05-21 14:52:59 -0400

    Technical Indicators Visualization

---
+++ indicators.py
@@ -0,0 +1,9 @@
+import plotly.graph_objs as go
+from ta.trend import MACD, EMAIndicator
+from ta.momentum import RSIIndicator
+from ta.volatility import AverageTrueRange, BollingerBands
+import pandas as pd
+
+def update_advanced_technical_graph(stock, start_date, end_date):
+    # Update advanced technical graph
+    pass
