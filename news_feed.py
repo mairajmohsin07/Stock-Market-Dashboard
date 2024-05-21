cc8cbeb076d73a88fce3e34c439bb78e3c67c788
mairajmohsin07 <mairaj.mohsin07@gmail.com>
2024-05-21 14:49:09 -0400

    News feed

---
+++ news_feed.py
@@ -0,0 +1,9 @@
+from newsapi import NewsApiClient
+import dash_html_components as html
+
+# Initialize NewsApiClient
+newsapi = NewsApiClient(api_key='YOUR_API_KEY')
+
+def news(stock):
+    # Fetch news articles
+    pass
